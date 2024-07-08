e80e2a5784c3d6709d36f2aea6eecd0e7f3b9bca
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-07-08 13:00:13 +0600

    about-section add

---
+++ js/student-buy.js
@@ -0,0 +1,41 @@
+const student_buy = new Splide("#student_buy", {
+  arrows: false,
+  type: "loop",
+  perPage: 3,
+  pagination: true,
+  gap: 10,
+  // autoplay: true,
+  interval: 2000,
+
+  perMove: 1,
+  classes: {
+    // Add classes for pagination.
+    pagination: "splide__pagination   ", // container
+    page: "splide__pagination__page    card__pagination", // each button
+  },
+  breakpoints: {
+    576: {
+      perPage: 2,
+      gap: 10,
+      perMove: 1,
+    },
+
+    768: {
+      perPoge: 2,
+      gap: 10,
+      perMove: 1,
+    },
+
+    992: {
+      perPage: 3,
+      perMove: 1,
+      focus: "none",
+    },
+
+    1024: {
+      perPage: 3,
+      gap: 10,
+    },
+  },
+});
+student_buy.mount();
